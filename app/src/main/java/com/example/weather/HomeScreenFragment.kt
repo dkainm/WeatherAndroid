df625dd6b94237cb8859ca8af56e812b39acb2dc
Vladislav Pavlov <nerly559@gmail.com>
2020-07-29 10:23:12 +0300

    Layout home screen. Add colors and fonts

---
+++ app/src/main/java/com/example/weather/HomeScreenFragment.kt
@@ -0,0 +1,22 @@
+package com.example.weather
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.home_screen_fragment.*
+
+class HomeScreenFragment: Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.home_screen_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
